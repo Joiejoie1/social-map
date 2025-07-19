7723d007fc1f33475a21065d16c4916aa837c98a
Joiejoie1 <soniqjjj@gmail.com>
2025-07-19 22:41:41 +0100

    Scaffolded React frontend with initial setup

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
+      <h1 className="text-white text-4xl font-bold">Tailwind is Working 🎉</h1>
+    </div>
+  );
+}
+
